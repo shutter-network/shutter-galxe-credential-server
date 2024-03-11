a8dc6a48cd8aa377cb3cf9a110438322c6162762
Jannik Luhn <jannik.luhn@posteo.de>
2024-03-11 12:16:39 +0100

    Listen on localhost

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 . /venv/bin/activate
 
-exec gunicorn --bind 0.0.0.0:5000 --forwarded-allow-ips='*' shutter_galxe_credential_server.main:create_app\(\)
+exec gunicorn --bind 127.0.0.1:5000 --forwarded-allow-ips='*' shutter_galxe_credential_server.main:create_app\(\)
